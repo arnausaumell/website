95e7c6c1be43a464bb0b254b5a20ea9e039b5cbc
arnausaumell <arnau.saumell@gmail.com>
2021-11-15 16:01:54 +0100

    First COMMIT

---
+++ app.js
@@ -0,0 +1,7 @@
+function logo_start(imageID) {
+    document.getElementById('imageID').style.opacity = "1";
+}
+
+function logo_stop(imageID) {
+    document.getElementById('imageID').style.opacity = "0";
+}
